4984142b41d3ea1c9e2fa093b2fca35f9c3ebeca
RSeguraHexe <rsegurah@unjbg.edu.pe>
2026-06-21 19:23:48 -0500

    Solucion, error al colocar un numero negativo o el cero como cantidad, del E1

---
+++ E1-V3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdlib>
+int main(){
+    int n=0, arreglo[1000];
+    while(n<=0){
+        system("cls");
+        std::cin>>n;
+    }
+    for(int i=0; i<n; i++){
+        std::cin>>arreglo[i];
+    }
+    for(int i=n-1; i>=0; i--){
+        std::cout<<arreglo[i]<<"\n";
+    }
+}
