317ef2af5ca67f9b6a40a914f5b3bc5298027205
RSeguraHexe <rsegurah@unjbg.edu.pe>
2026-06-22 15:21:26 -0500

    En el segundo for, la candicion hasta "i<=(n/2)" se ha cambiado por un "i<(n/2", del E2

---
+++ E2-V3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+int main(){
+    char cadena[20]; std::string c="P";
+    int n=0;
+    std::cin>>cadena;
+    for(int i=0; i<20; i++){
+        if(cadena[i]=='\0'){
+            n=i;    
+            break;
+        }
+    }
+    for(int i=0; i<(n/2); i++){
+        if(cadena[i]!=cadena[n-(i+1)]){
+            c="NP";
+            break;
+        }
+    }
+    std::cout<<c;
+}
